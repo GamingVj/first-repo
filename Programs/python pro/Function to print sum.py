3af3eb9a82a2f422bb07011f143252c8496bebae
Gaming Vj <gamingvj19@gmail.com>
2024-02-02 15:34:20 +0530

    changing files

--- Programs/python pro/Function to print sum.py
+++ Programs/python pro/Function to print sum.py
@@ -1,14 +1,17 @@
-'''
+"""
     Function to print sum
 ------------------------------------
 defining a  function name (dict)
-'''
-def returnSum(dict):
+"""
+
+
+def return_sum(dict):
     sum = 0
     for i in dict.values():
         sum = sum + i
 
     return sum
+
 
 '''
             Driver Function
@@ -20,7 +23,7 @@ a = int(input("Enter the 1st value out of 3\n-->"))
 b = int(input("Enter the 2nd value out of 3\n-->"))
 c = int(input("Enter the 3rd value out of 3\n-->"))
 
-#creating a dictionary with  key value pairs
+# creating a dictionary with  key value pairs
+
 mydict = {'a': a,'b': b,'c': c}
-print("Sum :",returnSum(mydict))
-    
+print("Sum :", return_sum(mydict))
