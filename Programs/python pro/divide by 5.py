6d55d3bf325f9116d58df7fd8c2836100b1018aa
Gaming Vj <gamingvj19@gmail.com>
2024-02-02 13:32:54 +0530

    changing py div file

--- Programs/python pro/divide by 5.py
+++ Programs/python pro/divide by 5.py
@@ -1,7 +1,7 @@
 # It is used to check if the number is divisble by 5 or not
 
 number=int(input("\nEnter a number\n-->"))
-
+#condition  for checking if the number is divisible by 5 or not. If it is then print that
 if number%5==0:
     print(f"{number} is divisble by 5\n")
 else:
