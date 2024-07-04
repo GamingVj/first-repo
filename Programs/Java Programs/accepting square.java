27a0aadcc3583ba0046bfdd6a66c1abdeb5b6496
Vijay Ingale <gamingvj19@gmail.com>
2024-07-04 22:22:42 +0530

    New Files

---
+++ Programs/Java Programs/accepting square.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class square
+{
+    public static void main(String args[])
+    {
+        int i, res;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("\n Enter a number");
+        i=sc.nextInt();
+        System.out.println("Square of the number ="+res);
+    }
+}
