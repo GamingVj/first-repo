55f8f157b12965770845b2ccc34c66febb625629
Vijay Ingale <gamingvj19@gmail.com>
2024-07-05 00:17:28 +0530

    Update Area of Circle.java

--- Programs/Java Programs/Area of Circle.java
+++ Programs/Java Programs/Area of Circle.java
@@ -3,7 +3,6 @@ class Area
 {
     public static void main(String args[])
     {
-        float a,r;
         Scanner sc=new Scanner(System.in);
         System.out.println("\n Enter the radius");
         double radius=sc.nextDouble();
