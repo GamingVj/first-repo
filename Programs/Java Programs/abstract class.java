67fecfa1889ff8c6dd29beff864b02ccf9a4c411
Vijay Ingale <gamingvj19@gmail.com>
2024-11-10 22:55:36 +0530

    Create abstract class.java

---
+++ Programs/Java Programs/abstract class.java
@@ -0,0 +1,51 @@
+abstract class abstract_class {
+    // Abstract method to make sound
+    abstract void makeSound();
+
+    // Abstract method to move
+    abstract void move();
+}
+
+class Dog extends abstract_class {
+    // Implementing the abstract method makeSound
+    @Override
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+
+    // Implementing the abstract method move
+    @Override
+    void move() {
+        System.out.println("Dog runs");
+    }
+}
+
+class Bird extends abstract_class {
+    // Implementing the abstract method makeSound
+    @Override
+    void makeSound() {
+        System.out.println("Bird chirps");
+    }
+
+    // Implementing the abstract method move
+    @Override
+    void move() {
+        System.out.println("Bird flies");
+    }
+}
+
+public class AbstractClassAndMethodsExample {
+    public static void main(String[] args) {
+        // Creating instances of Dog and Bird
+        abstract_class dog = new Dog();
+        abstract_class bird = new Bird();
+
+        // Calling methods on Dog instance
+        dog.makeSound();
+        dog.move();
+
+        // Calling methods on Bird instance
+        bird.makeSound();
+        bird.move();
+    }
+}
