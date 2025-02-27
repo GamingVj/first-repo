510eedde1a298900d73de4f7b05446e4d13f58d7
Vijay Ingale <gamingvj19@gmail.com>
2025-02-28 02:19:04 +0530

    Create 3 - color change.vb

---
+++ Programs/VB Code/3 - color change.vb
@@ -0,0 +1,13 @@
+' AIM : According to Click event of button it should change forms back ground color (Green, Yellow, Red on color button)
+
+Private Sub cmdgreen_click ()
+	form1.BackColor =  vbGreen
+End Sub
+
+Private Sub cmdred_click ()
+	form1.BackColor = vbRed
+End Sub
+
+Private Sub cmdyellow_click ()
+	form1.BackColor = vbyellow
+End Sub
