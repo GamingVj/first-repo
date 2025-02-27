e0d9336166b351dc3fb5f9bcd02e79e6cc44c0d5
Vijay Ingale <gamingvj19@gmail.com>
2025-02-28 02:19:08 +0530

    Create 5 - Positive or negative.vb

---
+++ Programs/VB Code/5 - Positive or negative.vb
@@ -0,0 +1,13 @@
+' AIM: To identify Whether the entered number is positive or negative.
+
+Private Sub cmdcheck_Click ()
+	Dim a As Integer
+	a = txtone.Text
+	If a>0 Then
+	Msgbox("The number you entered is positive")
+	Else if a<0 Then
+	Msgbox("The number you entered is negative")
+	Else
+	Msgbox("You have entered zero which is neither positive or negative")
+	Else if
+End Sub
