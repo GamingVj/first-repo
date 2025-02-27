503c2e79b952dd0fb45e40836c92e33b24927f0a
Vijay Ingale <gamingvj19@gmail.com>
2025-02-28 02:18:59 +0530

    Create 1 - change label.vb

---
+++ Programs/VB Code/1 - change label.vb
@@ -0,0 +1,15 @@
+' AIM: Change of label by using object and command button control.
+
+Private Sub CMDBACK_Click ()
+	LBLONE_Caption = " Computer Science Paper I"
+End Sub
+
+Private Sub CMDBACK_Click ()
+	LBLONE_Caption = "Computer Science Paper II"
+End Sub
+
+Private Sub CMDEXIT_Click ()
+	End
+End Sub
+
+
