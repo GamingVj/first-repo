0342b6d9df8fa6129aee000905d3b985a9bb1469
Vijay Ingale <88636683+GamingVj@users.noreply.github.com>
2025-09-03 19:01:46 +0530

    Add BookList2 and BookList3 components
    Introduced BookList2 and BookList3 components to demonstrate rendering book items with and without using map. Updated App.js to display both components and added related CSS styles for book and booklist layouts.

--- Programs/Html, Css and Js Files/React/my-app/src/App.js
+++ Programs/Html, Css and Js Files/React/my-app/src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Booklist from './Booklist';
+import BookList2 from './Booklist2';
+import BookList3 from './Booklist3';
 
 function App() {
   return (
     <div className="App">
-      <Booklist />
+      {/* <Booklist /> */}
+      <h2>This is without using map </h2>
+      <BookList2 />
+      <h2>This is with using map </h2>
+      <BookList3 />
+
     </div>
   );
 }
